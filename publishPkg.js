8daa6e235f49995de9ec78b72df87010eb9b1123
ashwinibm <ashwini@baysidetech.in>
2018-05-24 20:49:10 +0530

    Sync docs WIP

--- publishPkg.js
+++ publishPkg.js
@@ -2,8 +2,11 @@ const axios = require("axios");
 const path = require("path");
 const extract = require("extract-zip");
 const mkdirp = require("mkdirp");
+const fs = require("fs-extra");
 const constants = require("./constants");
 const qh = require("./utils/QueueHelper");
+const uh = require("./utils/UriHelper");
+const sh = require("./utils/ServiceHelper");
 
 const unzip = (src, dest) => {
   return new Promise(function(resolve, reject) {
@@ -15,8 +18,35 @@ const unzip = (src, dest) => {
 }
 
 /**
+ * Reads a given file to string.
+ */
+const readFile = (filename) => {
+  return new Promise(function(resolve, reject) {
+    fs.readFile(filename, "utf8", function(err, data) {
+      if (err) reject(err);
+      else resolve(data);
+    });
+  });
+}
+
+/**
+ * Syncs doc with gawati-data
+ */
+const syncPkg = (xmlPackage) => {
+  console.log(" IN: syncPkg");
+  const syncPkgApi = sh.getApi("xmlServer", "syncPkg");
+  const {url, method} = syncPkgApi;
+  return axios({
+      method: method,
+      url: url,
+      data: xmlPackage
+  })
+}
+
+/**
  * Extract zip and sync docs with gawati-data.
  * Publish status of pkg processed by the gawati-data.
+ * To-Do: Status fail messages
  */
 const toGawatiData = (zipObj) => {
   console.log(" IN: toGawatiData");
@@ -24,12 +54,27 @@ const toGawatiData = (zipObj) => {
   const targetName = src.split("/").pop().split(".")[0];
   const dest = constants.TMP_AKN_FOLDER();
 
+  //XML doc name and path
+  const docName = uh.fileNameFromIRI(iri, "xml");
+  const docPath = path.join(constants.TMP_AKN_FOLDER(), targetName, docName);
+
   //Extract zip
   unzip(src, path.resolve(dest))
   .then((res) => {
     console.log("Extracted to ", targetName);
-    //Sync docs with gawati-data
-
+    return readFile(docPath);
+  })
+  .then((data) => {
+    //To-Do: Extract <an:akomaNtoso> section
+    xmlPackage = {
+      "fileXml": docName,
+      "iri": iri,
+      "data": data
+    };
+    return syncPkg(xmlPackage);
+  })
+  .then((res) => {
+    console.log(res.data); //To-Do: Check code for errors
     //Publish on STATUS_Q
     qh.publishStatus(qh.formMsg(iri, 'published', 'Published on Gawati-Data'));
   })
---
+++ utils/UriHelper.js
@@ -0,0 +1,30 @@
+/**
+ * Generates a normalized file name from a FRBRExpression/FRBRthis/@value iri
+ */
+const fileNameFromIRI = (iri, fileType) => {
+    let filePrefix = fileNamePrefixFromIRI(iri);
+    return `${filePrefix}.${fileType}`;
+};
+
+const fileNamePrefixFromIRI = (iri) => {
+    let iriArr = iri.split("/");
+    iriArr.shift();
+    let filePrefix = iriArr.map(
+        (item) => {
+            if (item.indexOf("@") !== -1) {
+                if (item.endsWith("@")) {
+                    return item.replace("@", "");
+                } else {
+                    return item.replace("@", "_");
+                } 
+            } else {
+                return item.replace("!", "");
+            }
+        }
+    ).join("_");
+    return filePrefix;
+};
+
+module.exports = {
+  fileNameFromIRI: fileNameFromIRI
+}
