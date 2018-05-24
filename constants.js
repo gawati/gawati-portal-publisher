969165af08815b8979e4ef8f9da07d7d7403e2f2
ashwinibm <ashwini@baysidetech.in>
2018-05-24 14:10:04 +0530

    Extract zip

---
+++ constants.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const API_PROTOCOL = process.env.API_PROTOCOL || 'http' ;
+const API_HOST = process.env.API_HOST || 'localhost' ;
+const API_PORT = process.env.API_PORT || '8080' ;
+
+/** Folders */
+const TMP_AKN_FOLDER = () => path.join(".", "tmp");
+
+const API_SERVER_BASE = () =>
+    API_PROTOCOL + '://' + API_HOST + ":" + API_PORT + '/exist/restxq';
+
+const PROCESS_NAME = "GAWATI-PORTAL-PUBLISHER";
+
+module.exports = {
+    TMP_AKN_FOLDER: TMP_AKN_FOLDER,
+    API_SERVER_BASE: API_SERVER_BASE,
+    PROCESS_NAME: PROCESS_NAME
+};
--- publishPkg.js
+++ publishPkg.js
@@ -1,4 +1,26 @@
 const axios = require("axios");
+const path = require("path");
+const extract = require("extract-zip");
+const mkdirp = require("mkdirp");
+const constants = require("./constants");
+
+const unzip = (src, dest) => {
+  return new Promise(function(resolve, reject) {
+    extract(src, {dir: dest}, function(err) {
+      if (err) reject(err);
+      else resolve(true);
+    })
+  });
+}
+
+const publishStatus = (msg) => {
+  const mq = require("./queues");
+  const qName = 'STATUS_Q';
+  const ex = mq.getExchange();
+  const key = mq.getQKey(qName);
+  mq.getChannel(qName).publish(ex, key, new Buffer(JSON.stringify(msg)));
+  console.log(" Status published on STATUS_Q");
+}
 
 /**
  * Extract zip and sync docs with gawati-data.
@@ -6,21 +28,24 @@ const axios = require("axios");
  */
 const toGawatiData = (zipObj) => {
   console.log(" IN: toGawatiData");
+  const {iri, zipPath: src} = zipObj;
+  const targetName = src.split("/").pop().split(".")[0];
+  const dest = constants.TMP_AKN_FOLDER();
+
   //Extract zip
+  unzip(src, path.resolve(dest))
+  .then((res) => {
+    console.log("Extracted to ", targetName);
+    //Sync docs with gawati-data
 
-  //Sync docs with gawati-data
-
-  //Publish on STATUS_Q
-  const msg = {
-    "iri": zipObj.iri,
-    "status": "published" 
-  }
-  const mq = require("./queues");
-  const qName = 'STATUS_Q';
-  const ex = mq.getExchange();
-  const key = mq.getQKey(qName);
-  mq.getChannel(qName).publish(ex, key, new Buffer(JSON.stringify(msg)));
-  console.log(" Status published on STATUS_Q");
+    //Publish on STATUS_Q
+    const msg = {
+      "iri": zipObj.iri,
+      "status": "published"
+    }
+    publishStatus(msg);
+  })
+  .catch(err => console.log(err));
 };
 
 module.exports.toGawatiData = toGawatiData;
